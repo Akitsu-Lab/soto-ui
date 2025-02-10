6db85d33bca039b73890afd059b0cea77ef89354
a163236 <nagakeita12@gmail.com>
2025-02-10 21:04:56 +0900

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,87 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client';
+import {Button, DataTable, Table, TableBody, TableCell, TableHead, TableHeader, TableRow} from "@carbon/react";
+import axios from "axios";
+
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    const instance = axios.create({
+        baseURL: "https://soto-account.deno.dev",
+        headers: {
+            'Content-Type': 'application/json',
+            "Access-Control-Allow-Origin": "*",
+        },
+    })
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+    instance.get('/accounts').then(function (response) {
+        console.log(response);
+    }).catch(function (error) {
+        console.log(error);
+    })
+
+    async function fetchDataTable() {
+        try {
+            const rows = await axios.get('/accounts');
+            console.log(rows);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const rows = [
+        {
+            id: '9',
+            name: 'kankurou',
+            balance: '0',
+        }
+    ];
+
+    const headers = [
+        {
+            key: 'id',
+            header: 'ID',
+        },
+        {
+            key: 'name',
+            header: 'アカウント名',
+        },
+        {
+            key: 'balance',
+            header: '残高',
+        }
+    ];
+
+    return (
+        <div>
+            <main>
+                <Button>a</Button>
+                <DataTable rows={rows} headers={headers}>
+                    {({rows, headers, getTableProps, getHeaderProps, getRowProps}) => (
+                        <Table {...getTableProps()}>
+                            <TableHead>
+                                <TableRow>
+                                    {headers.map((header) => (
+                                        <TableHeader {...getHeaderProps({header})} key={header.key}>
+                                            {header.header}
+                                        </TableHeader>
+                                    ))}
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {rows.map((row) => (
+                                    <TableRow {...getRowProps({row})} key={row.id}>
+                                        {row.cells.map((cell) => (
+                                            <TableCell key={cell.id}>{cell.value}</TableCell>
+                                        ))}
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    )}
+                </DataTable>
+            </main>
+            <footer>
+
+            </footer>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    );
 }
